177c08674e449add905886c8ba07be46bece12ea
SAYAN MAITY <sayanmaity007@gmail.com>
2020-06-14 21:45:58 +0530

    signout and profile name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery'
+import * as jwt_decode from 'jwt-decode';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,14 +10,19 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   isModalOn: boolean = false;
-  constructor(private _router:Router) {}
-
+  constructor(private _router: Router) {}
+  user_name = ''
   ngOnInit(): void {
+    this.user_name = jwt_decode( localStorage.getItem('token')).user_name
     $(document).ready(function () {
       $('#toggle-btn').click(function () {
         $('.sidebar').toggleClass('show');
       });
     });
   }
-  
+
+  signOut(){
+    localStorage.clear()
+    this._router.navigate(['/sign-in'])
+  }
 }
